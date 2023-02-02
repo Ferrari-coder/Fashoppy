f90618a5f798fc5a7e46fc5c4a2694878021dc6a
Ferrari-coder <oluwaferanmiaiyegbusi@gmail.com>
2023-02-02 17:16:46 +0100

    all pages except reg,login and cart page done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Product from './pages/Product';
+// import Home from './pages/Home';
+// import ProductList from './pages/ProductList';
+
+function App() {
+ return (
+  <Product/>
+ )
+}
+
+export default App;
