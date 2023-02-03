f6a190a99c6a46c4147987b60d0447f21357af49
Ferrari-coder <oluwaferanmiaiyegbusi@gmail.com>
2023-02-03 19:40:49 +0100

    working on cart page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import Product from './pages/Product';
+import Cart from './pages/Cart';
+// import Login from './pages/Login';
+// import Product from './pages/Product';
+// import Register from './pages/Register';
 // import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
 
 function App() {
  return (
-  <Product/>
+  <Cart/>
  )
 }
 
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+
+  &:hover {
+    border: ${(props) =>
+      props.type === "filled" ? "2px solid black" : "none"};
+    background-color: ${(props) =>
+      props.type === "filled" ? "transparent" : "black"};
+    color: ${(props) => (props.type === "filled" ? "black" : "white")};
+  }
+`;
+
+const TopTexts = styled.div``;
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+`;
+
+const Product = styled.div``
+const ProductDetail = styled.div``
+const Image = styled.img``
+const Details = styled.div``
+const ProductName = styled.div``
+const ProductId = styled.div``
+const ProductColor = styled.div``
+const ProductSize = styled.div``
+const PriceDetail = styled.div``
+
+
+
+const Cart = () => {
+  return (
+    <Container>
+      <Announcement />
+      <Navbar />
+      <Wrapper>
+        <Title>YOUR BAG</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>Shopping Bag(2)</TopText>
+            <TopText>Your Wishlist (0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT NOW</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.pinimg.com/originals/2d/af/f8/2daff8e0823e51dd752704a47d5b795c.png" />
+                <Details>
+                  <ProductName>
+                    <b>Product:</b> HAKURA T-SHIRT
+                  </ProductName>
+                  <ProductId>
+                    <b>ID:</b> 93813718293
+                  </ProductId>
+                  <ProductColor color="gray" />
+                  <ProductSize>
+                    <b>Size:</b> M
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail></PriceDetail>
+            </Product>
+          </Info>
+          <Summary></Summary>
+        </Bottom>
+      </Wrapper>
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
